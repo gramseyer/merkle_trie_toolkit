c46bb4d2f03b0c83b43ded14ef08e7c9fe26649f
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-05-26 17:51:52 -0700

    switching to catch2

---
+++ tests/test_serialize.cc
@@ -0,0 +1,37 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <cstdint>
+#include <cstdio>
+
+#include "mtt/utils/serialize_endian.h"
+
+namespace utils
+{
+
+TEST_CASE("write big endian", "[utils]")
+{
+	uint8_t buf[4];
+	uint32_t val = 0x01020304;
+
+	write_unsigned_big_endian(buf, val);
+
+	REQUIRE(buf[0] == 0x01);
+	REQUIRE(buf[1] == 0x02);
+	REQUIRE(buf[2] == 0x03);
+	REQUIRE(buf[3] == 0x04);
+}
+
+TEST_CASE("write little endian", "[utils]")
+{
+	uint8_t buf[4];
+	uint32_t val = 0x01020304;
+
+	write_unsigned_little_endian(buf, val);
+
+	REQUIRE(buf[0] == 0x04);
+	REQUIRE(buf[1] == 0x03);
+	REQUIRE(buf[2] == 0x02);
+	REQUIRE(buf[3] == 0x01);
+}
+
+} /* utils */
