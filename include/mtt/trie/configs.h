ca470c13bd4215a3ba804f50f972324d407aa113
gramseyer <ramseyer@uchicago.edu>
2022-10-15 17:06:48 +0000

    fix prints

--- include/mtt/trie/configs.h
+++ include/mtt/trie/configs.h
@@ -14,10 +14,15 @@ constexpr static bool TRIE_LOG_HASH_RECORDS = (_LOG_HASH_RECORDS != 0);
 
 namespace detail
 {
-	// when in a header that gets included by many translation units,
-	// log_configs() gets called many times, instead of just once,
-	// leading to many repeated printouts (annoying)
-	inline static std::atomic<bool> configs_printed = false;
+	// need struct namespace to ensure only one instance of the static 
+	// variable globally
+	struct ConfigsPrinted
+	{
+		// when in a header that gets included by many translation units,
+		// log_configs() gets called many times, instead of just once,
+		// leading to many repeated printouts (annoying)
+		inline static std::atomic<int32_t> configs_printed = 0;
+	};
 }
 
 [[maybe_unused]]
@@ -25,7 +30,8 @@ static void
 __attribute__((constructor))
 log_configs()
 {
-	if (!detail::configs_printed.exchange(true))
+	int32_t res = detail::ConfigsPrinted::configs_printed.fetch_add(1);
+	if (res == 0)
 	{
 		std::printf("========== trie configs ==========\n");
 		std::printf("TRIE_LOG_HASH_RECORDS         = %u\n", TRIE_LOG_HASH_RECORDS);
