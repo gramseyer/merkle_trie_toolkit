e7b713074b1c2e182abcc2594582f2ffc48c1503
Geoff Ramseyer <gdwr@cs.stanford.edu>
2023-06-28 14:18:09 -0700

    additional tests, reformat file

--- tests/test_atomic_merkle_trie.cc
+++ tests/test_atomic_merkle_trie.cc
@@ -7,252 +7,398 @@
 #include "mtt/common/debug_macros.h"
 
 #include "mtt/snapshot_trie/atomic_merkle_trie.h"
-#include "mtt/trie/utils.h"
 #include "mtt/trie/types.h"
+#include "mtt/trie/utils.h"
 
 #include <utils/serialize_endian.h>
-#include <utils/time.h>
 #include <utils/threadlocal_cache.h>
+#include <utils/time.h>
 
 #include <tbb/parallel_for.h>
 
-namespace trie
-{
-
+namespace trie {
 
 TEST_CASE("basic get subnode ref and normalize", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
 
-	mt m;
+    mt m;
 
-	REQUIRE(m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(32)) != nullptr);
-	REQUIRE(m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFFFF'FFFF'0000'0000), PrefixLenBits(32)) != nullptr);
-	auto* p0 = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFFFF'0000'0000'0000), PrefixLenBits(16));
+    REQUIRE(m.get_subnode_ref_and_invalidate_hash(
+                UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(32))
+            != nullptr);
+    REQUIRE(m.get_subnode_ref_and_invalidate_hash(
+                UInt64Prefix(0xFFFF'FFFF'0000'0000), PrefixLenBits(32))
+            != nullptr);
+    auto* p0 = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0xFFFF'0000'0000'0000), PrefixLenBits(16));
 
-	auto* p1 = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFFFF'AAAA'0000'0000), PrefixLenBits(32));
-	auto* p2 = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFFFF'AAAA'0000'0000), PrefixLenBits(32));
+    auto* p1 = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0xFFFF'AAAA'0000'0000), PrefixLenBits(32));
+    auto* p2 = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0xFFFF'AAAA'0000'0000), PrefixLenBits(32));
 
-	REQUIRE(p1 == p2);
+    REQUIRE(p1 == p2);
 
-	REQUIRE(p0 -> get_child(0xA) == p1);
+    REQUIRE(p0->get_child(0xA) == p1);
 }
 
 TEST_CASE("normalize cleans up in case of no inserts", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
 
-	mt m;
+    mt m;
 
-	REQUIRE(m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(32)) != nullptr);
-	REQUIRE(m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFFFF'FFFF'0000'0000), PrefixLenBits(32)) != nullptr);
-	REQUIRE(m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFFFF'0000'0000'0000), PrefixLenBits(24)) != nullptr);
-	REQUIRE(m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0xFF00'FF00'0000'0000), PrefixLenBits(16)) != nullptr);
+    REQUIRE(m.get_subnode_ref_and_invalidate_hash(
+                UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(32))
+            != nullptr);
+    REQUIRE(m.get_subnode_ref_and_invalidate_hash(
+                UInt64Prefix(0xFFFF'FFFF'0000'0000), PrefixLenBits(32))
+            != nullptr);
+    REQUIRE(m.get_subnode_ref_and_invalidate_hash(
+                UInt64Prefix(0xFFFF'0000'0000'0000), PrefixLenBits(24))
+            != nullptr);
+    REQUIRE(m.get_subnode_ref_and_invalidate_hash(
+                UInt64Prefix(0xFF00'FF00'0000'0000), PrefixLenBits(16))
+            != nullptr);
 
-	auto h1 = m.hash_and_normalize();
+    auto h1 = m.hash_and_normalize();
 
-	mt m2;
-	auto h2 = m2.hash_and_normalize();
+    mt m2;
+    auto h2 = m2.hash_and_normalize();
 
-	REQUIRE(h1 == h2);
+    REQUIRE(h1 == h2);
 }
 
-void overwrite_merge_fn(EmptyValue& a, const EmptyValue& b) {}
+struct Uint8Wrapper
+{
+    uint8_t value;
+
+    Uint8Wrapper(uint8_t const& v)
+        : value(v)
+    {}
+
+    void copy_data(std::vector<uint8_t>& bytes) { bytes.push_back(value); }
+};
+
+void
+overwrite_merge_fn(EmptyValue& a, const EmptyValue& b)
+{}
+void
+overwrite_merge_fn_uint8(Uint8Wrapper& a, const Uint8Wrapper& b)
+{
+    a = b;
+}
+
+TEST_CASE("check insert after getsubnode on value", "[amt]")
+{
+    using mt = AtomicMerkleTrie<UInt64Prefix, Uint8Wrapper, 256>;
+
+    mt m;
+
+    auto* ptr = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(64));
+
+    REQUIRE(ptr != nullptr);
+
+    ptr->template insert<&overwrite_merge_fn_uint8>(
+        UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), 1);
+    ptr->template insert<&overwrite_merge_fn_uint8>(
+        UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), 2);
+
+    auto h1 = m.hash_and_normalize();
+
+    mt m2;
+
+    m2.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(0))
+        ->template insert<&overwrite_merge_fn_uint8>(
+            UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), 2);
+
+    auto h2 = m2.hash_and_normalize();
+
+    REQUIRE(h1 == h2);
+}
+
+TEST_CASE("check delete after getsubnode on value", "[amt]")
+{
+    using mt = AtomicMerkleTrie<UInt64Prefix, Uint8Wrapper, 256>;
+
+    mt m;
+
+    auto* ptr = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(64));
+
+    REQUIRE(ptr != nullptr);
+
+    ptr->template insert<&overwrite_merge_fn_uint8>(
+        UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), 1);
+
+    auto h1 = m.hash_and_normalize();
+    // WARNING -- reusing pointers from get_subnode_ref doesn't invalidate hashes again
+
+	ptr = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(64));
+	REQUIRE(ptr != nullptr);
+	
+    ptr -> delete_value(UInt64Prefix(0x0000'0000'0000'0000), m.get_gc());
+
+    auto h11 = m.hash_and_normalize();
+
+    mt m2;
+
+    auto h2 = m2.hash_and_normalize();
+
+    REQUIRE(h1 != h2);
+    REQUIRE(h11 == h2);
+}
+
+TEST_CASE("check cleanup after get_subnode on value", "[amt]")
+{
+	using mt = AtomicMerkleTrie<UInt64Prefix, Uint8Wrapper, 256>;
+
+    mt m;
+
+    auto* ptr = m.get_subnode_ref_and_invalidate_hash(
+        UInt64Prefix(0x0000'0000'0000'0000), PrefixLenBits(64));
+
+    REQUIRE(ptr != nullptr);
+
+    auto h1 = m.hash_and_normalize();
+
+    mt m2;
+
+    auto h2 = m2.hash_and_normalize();
+
+    REQUIRE(h1 == h2);
+}
 
 TEST_CASE("check inserts", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
 
-	mt m;
+    mt m;
 
-	auto* root = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(0));
+    auto* root = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                                       PrefixLenBits(0));
 
-	for (uint64_t i = 0; i < 1000; i++)
-	{
-		uint64_t query = (i * 17) % 6701;  //6701 is prime
+    for (uint64_t i = 0; i < 1000; i++) {
+        uint64_t query = (i * 17) % 6701; // 6701 is prime
 
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(query), m.get_gc(), EmptyValue{});
-	}
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(query), m.get_gc(), EmptyValue{});
+    }
 
-	auto h1 = m.hash_and_normalize();
+    auto h1 = m.hash_and_normalize();
 
-	for (uint64_t i = 0; i < 1000; i++)
-	{
-		uint64_t query = ((i + 1000) * 17) % 6701;  //6701 is prime
+    for (uint64_t i = 0; i < 1000; i++) {
+        uint64_t query = ((i + 1000) * 17) % 6701; // 6701 is prime
 
-		m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(query), PrefixLenBits(56));
-	}
+        m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(query),
+                                              PrefixLenBits(56));
+    }
 
-	auto h2 = m.hash_and_normalize();
+    auto h2 = m.hash_and_normalize();
 
-	REQUIRE(h1 == h2);
-
+    REQUIRE(h1 == h2);
 }
 TEST_CASE("force recompute", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
 
-	mt m;
+    mt m;
 
-	auto h1 = m.hash_and_normalize();
+    auto h1 = m.hash_and_normalize();
 
-	auto* base = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(60));
+    auto* base = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                                       PrefixLenBits(60));
 
-	base -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0), m.get_gc(), EmptyValue{});
+    base->template insert<&overwrite_merge_fn>(
+        UInt64Prefix(0x0), m.get_gc(), EmptyValue{});
 
-	auto h2 = m.hash_and_normalize();
+    auto h2 = m.hash_and_normalize();
 
-	REQUIRE(h1 != h2);
+    REQUIRE(h1 != h2);
 
-	base -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0), m.get_gc(), EmptyValue{});
+    base->template insert<&overwrite_merge_fn>(
+        UInt64Prefix(0x0), m.get_gc(), EmptyValue{});
 
-	REQUIRE(h2 == m.hash_and_normalize());
+    REQUIRE(h2 == m.hash_and_normalize());
 }
 
 TEST_CASE("ensure full length key", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
 
-	mt m;
+    mt m;
 
-	auto h = m.hash_and_normalize();
+    auto h = m.hash_and_normalize();
 
-	auto* base = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(64));
+    auto* base = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                                       PrefixLenBits(64));
 
-	base -> template insert<&overwrite_merge_fn>(UInt64Prefix(0), m.get_gc(), EmptyValue{});
+    base->template insert<&overwrite_merge_fn>(
+        UInt64Prefix(0), m.get_gc(), EmptyValue{});
 
-	auto h2 = m.hash_and_normalize();
+    auto h2 = m.hash_and_normalize();
 
-	REQUIRE(h != h2);
+    REQUIRE(h != h2);
 }
 
-TEST_CASE("get proper length subnode")
+TEST_CASE("get proper length subnode", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
-	mt m;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    mt m;
 
-	using prefix_t = UInt64Prefix;
+    using prefix_t = UInt64Prefix;
 
-	SECTION("from empty")
-	{
-		auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0), PrefixLenBits(0));
-		REQUIRE(b -> get_prefix_len().len == 0);
+    SECTION("from empty")
+    {
+        auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0),
+                                                        PrefixLenBits(0));
+        REQUIRE(b->get_prefix_len().len == 0);
 
-		b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0), PrefixLenBits(28));
+        b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0),
+                                                  PrefixLenBits(28));
 
-		REQUIRE(b -> get_prefix_len().len == 28);
+        REQUIRE(b->get_prefix_len().len == 28);
 
-		b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0), PrefixLenBits(64));
+        b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0),
+                                                  PrefixLenBits(64));
 
-		REQUIRE(b -> get_prefix_len().len == 64);
-	}
-	SECTION("from singlechild")
-	{
-		{
-			auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0), PrefixLenBits(0));
-			b -> template insert<&overwrite_merge_fn>(prefix_t(0x0000'0000'0000'0000), m.get_gc(), EmptyValue{});
-			b -> template insert<&overwrite_merge_fn>(prefix_t(0x1000'0000'0000'0000), m.get_gc(), EmptyValue{});
-		}
+        REQUIRE(b->get_prefix_len().len == 64);
+    }
+    SECTION("from singlechild")
+    {
+        {
+            auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0),
+                                                            PrefixLenBits(0));
+            b->template insert<&overwrite_merge_fn>(
+                prefix_t(0x0000'0000'0000'0000), m.get_gc(), EmptyValue{});
+            b->template insert<&overwrite_merge_fn>(
+                prefix_t(0x1000'0000'0000'0000), m.get_gc(), EmptyValue{});
+        }
 
-		auto h1 = m.hash_and_normalize();
+        auto h1 = m.hash_and_normalize();
 
-		auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0), PrefixLenBits(0));
-		REQUIRE(b -> get_prefix_len().len == 0);
+        auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0),
+                                                        PrefixLenBits(0));
+        REQUIRE(b->get_prefix_len().len == 0);
 
-		b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0x1000'0000'0000'0000), PrefixLenBits(4));
-		REQUIRE(b -> get_prefix_len().len == 4);
+        b = m.get_subnode_ref_and_invalidate_hash(
+            prefix_t(0x1000'0000'0000'0000), PrefixLenBits(4));
+        REQUIRE(b->get_prefix_len().len == 4);
 
+        b = m.get_subnode_ref_and_invalidate_hash(
+            prefix_t(0x1000'0000'0000'0000), PrefixLenBits(56));
+        REQUIRE(b->get_prefix_len().len == 56);
 
-		b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0x1000'0000'0000'0000), PrefixLenBits(56));
-		REQUIRE(b -> get_prefix_len().len == 56);
+        b = m.get_subnode_ref_and_invalidate_hash(
+            prefix_t(0x2000'0000'0000'0000), PrefixLenBits(56));
+        REQUIRE(b->get_prefix_len().len == 56);
 
-		b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0x2000'0000'0000'0000), PrefixLenBits(56));
-		REQUIRE(b -> get_prefix_len().len == 56);
+        b = m.get_subnode_ref_and_invalidate_hash(
+            prefix_t(0x1100'0000'0000'0000), PrefixLenBits(56));
+        REQUIRE(b->get_prefix_len().len == 56);
 
-		b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0x1100'0000'0000'0000), PrefixLenBits(56));
-		REQUIRE(b -> get_prefix_len().len == 56);
+        REQUIRE(m.hash_and_normalize() == h1);
+    }
 
-		REQUIRE(m.hash_and_normalize() == h1);
-	}
+    SECTION("from middle")
+    {
+        {
+            auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0),
+                                                            PrefixLenBits(0));
+            b->template insert<&overwrite_merge_fn>(
+                prefix_t(0xFFFF'0000'0000'FFFF), m.get_gc(), EmptyValue{});
+            b->template insert<&overwrite_merge_fn>(
+                prefix_t(0xFFFF'0000'0000'0000), m.get_gc(), EmptyValue{});
+        }
 
-	SECTION("from middle")
-	{
-		{
-			auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0), PrefixLenBits(0));
-			b -> template insert<&overwrite_merge_fn>(prefix_t(0xFFFF'0000'0000'FFFF),  m.get_gc(), EmptyValue{});
-			b -> template insert<&overwrite_merge_fn>(prefix_t(0xFFFF'0000'0000'0000),  m.get_gc(), EmptyValue{});
-		}
+        auto h1 = m.hash_and_normalize();
 
-		auto h1 = m.hash_and_normalize();
+        auto* b = m.get_subnode_ref_and_invalidate_hash(
+            prefix_t(0x1000'0000'0000'0000), PrefixLenBits(4));
+        REQUIRE(b->get_prefix_len().len == 4);
 
-		auto* b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0x1000'0000'0000'0000), PrefixLenBits(4));
-		REQUIRE(b -> get_prefix_len().len == 4);
-
-		SECTION("extend common point")
-		{
-			b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0xFFFF'FFFF'0000'0000), PrefixLenBits(32));
-			REQUIRE(b -> get_prefix_len().len == 32);
-			REQUIRE(m.hash_and_normalize() == h1);
-		}
-		SECTION("at common point")
-		{
-			b = m.get_subnode_ref_and_invalidate_hash(prefix_t(0xFFFF'0000'0000'0000), PrefixLenBits(16));
-			REQUIRE(b -> get_prefix_len().len == 16);
-			REQUIRE(m.hash_and_normalize() == h1);
-		}
-	}
+        SECTION("extend common point")
+        {
+            b = m.get_subnode_ref_and_invalidate_hash(
+                prefix_t(0xFFFF'FFFF'0000'0000), PrefixLenBits(32));
+            REQUIRE(b->get_prefix_len().len == 32);
+            REQUIRE(m.hash_and_normalize() == h1);
+        }
+        SECTION("at common point")
+        {
+            b = m.get_subnode_ref_and_invalidate_hash(
+                prefix_t(0xFFFF'0000'0000'0000), PrefixLenBits(16));
+            REQUIRE(b->get_prefix_len().len == 16);
+            REQUIRE(m.hash_and_normalize() == h1);
+        }
+    }
 }
 
 TEST_CASE("deletions", "[amt]")
 {
-	using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
+    using mt = AtomicMerkleTrie<UInt64Prefix, EmptyValue, 256>;
 
-	mt m;
+    mt m;
 
-	auto h = m.hash_and_normalize();
+    auto h = m.hash_and_normalize();
 
-	auto* root = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(32));
+    auto* root = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                                       PrefixLenBits(32));
 
-	SECTION("single elt")
-	{
+    SECTION("single elt")
+    {
 
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), EmptyValue{});
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), EmptyValue{});
 
-		root -> delete_value(UInt64Prefix(0x0000'0000'0000'0000), m.get_gc());
+        root->delete_value(UInt64Prefix(0x0000'0000'0000'0000), m.get_gc());
 
-		REQUIRE(m.hash_and_normalize() == h);
-	}
+        REQUIRE(m.hash_and_normalize() == h);
+    }
 
-	SECTION("several elt")
-	{
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), EmptyValue{});
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0000'0000'0000'1111), m.get_gc(), EmptyValue{});
+    SECTION("several elt")
+    {
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(0x0000'0000'0000'0000), m.get_gc(), EmptyValue{});
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(0x0000'0000'0000'1111), m.get_gc(), EmptyValue{});
 
-		h = m.hash_and_normalize();
-		root = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(32));
+        h = m.hash_and_normalize();
+        root = m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                                     PrefixLenBits(32));
 
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0000'0000'0000'2222), m.get_gc(), EmptyValue{});
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(0x0000'0000'0000'2222), m.get_gc(), EmptyValue{});
 
-		root -> delete_value(UInt64Prefix(0x0000'0000'0000'2222), m.get_gc());
+        root->delete_value(UInt64Prefix(0x0000'0000'0000'2222), m.get_gc());
 
-		REQUIRE(m.hash_and_normalize() == h);
-	}
+        REQUIRE(m.hash_and_normalize() == h);
+    }
 
-	SECTION("cleanup chain")
-	{
-		m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(32));
-		m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(40));
-		m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(44));
-		m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0), PrefixLenBits(48));
+    SECTION("cleanup chain")
+    {
+        m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                              PrefixLenBits(32));
+        m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                              PrefixLenBits(40));
+        m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                              PrefixLenBits(44));
+        m.get_subnode_ref_and_invalidate_hash(UInt64Prefix(0),
+                                              PrefixLenBits(48));
 
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0000'0000'0000'2222), m.get_gc(), EmptyValue{});
-		root -> template insert<&overwrite_merge_fn>(UInt64Prefix(0x0000'0000'0000'1111), m.get_gc(), EmptyValue{});
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(0x0000'0000'0000'2222), m.get_gc(), EmptyValue{});
+        root->template insert<&overwrite_merge_fn>(
+            UInt64Prefix(0x0000'0000'0000'1111), m.get_gc(), EmptyValue{});
 
-		root -> delete_value(UInt64Prefix(0x0000'0000'0000'2222), m.get_gc());
-		root -> delete_value(UInt64Prefix(0x0000'0000'0000'1111), m.get_gc());
+        root->delete_value(UInt64Prefix(0x0000'0000'0000'2222), m.get_gc());
+        root->delete_value(UInt64Prefix(0x0000'0000'0000'1111), m.get_gc());
 
-		REQUIRE(m.hash_and_normalize() == h);
-	}
+        REQUIRE(m.hash_and_normalize() == h);
+    }
 }
 
 } // namespace trie
