eaecc9ab70a22f35e77ea393ed9538b35dce2acc
Geoff Ramseyer <gdwr@cs.stanford.edu>
2023-09-08 14:22:36 -0700

    format

--- include/mtt/snapshot_trie/optional_value.h
+++ include/mtt/snapshot_trie/optional_value.h
@@ -2,70 +2,55 @@
 
 #include <optional>
 
-namespace trie
-{
+namespace trie {
 
 /**
- * Wrapper class to inject extra validation criteria on whether a value should exist
- * within AtomicMerkleTrie
+ * Wrapper class to inject extra validation criteria on whether a value should
+ *exist within AtomicMerkleTrie
  **/
 
 namespace detail {
-	template<typename T>
-	auto default_value_selector = [] (T const&) -> bool { return true; };
+template<typename T>
+auto default_value_selector = [](T const&) -> bool { return true; };
 }
 
-template<typename value_t, auto has_value_f = detail::default_value_selector<value_t>>
+template<typename value_t,
+         auto has_value_f = detail::default_value_selector<value_t>>
 class OptionalValue
 {
-	std::optional<value_t> base;
+    std::optional<value_t> base;
 
-public:
-	using value_type = value_t;
+  public:
+    using value_type = value_t;
 
-	template<typename... T>
-	OptionalValue(T&&... args)
-		: base(std::forward<T>(args)...)
-	{}
+    template<typename... T>
+    OptionalValue(T&&... args)
+        : base(std::forward<T>(args)...)
+    {}
 
-	template<typename... T>
-	void emplace(T&&... args)
-	{
-		base.emplace(std::forward<T>(args)...);
-	}
+    template<typename... T>
+    void emplace(T&&... args)
+    {
+        base.emplace(std::forward<T>(args)...);
+    }
 
-	void reset()
-	{
-		base.reset();
-	}
+    void reset() { base.reset(); }
 
-	value_t& operator*()
-	{
-		return *base;
-	}
+    value_t& operator*() { return *base; }
 
-	const value_t& operator*() const
-	{
-		return *base;
-	}
+    const value_t& operator*() const { return *base; }
 
-	value_t* operator->()
-	{
-		return &(*base);
-	}
+    value_t* operator->() { return &(*base); }
 
-	const value_t* operator->() const
-	{
-		return &(*base);
-	}
+    const value_t* operator->() const { return &(*base); }
 
-	bool has_value() const {
-		if (!base) {
-			return false;
-		}
-		return has_value_f(*base);
-	}
+    bool has_value() const
+    {
+        if (!base) {
+            return false;
+        }
+        return has_value_f(*base);
+    }
 };
 
-}
-
+} // namespace trie
